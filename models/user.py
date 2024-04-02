14cba5648d9262934ba905646e156e807ac56cac
ETG1 <elijahkazungu22@gmail.com>
2024-04-01 20:44:31 -0700

    Updates

--- models/user.py
+++ models/user.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" holds class User"""
+''' holds class User '''
+
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    ''' Representation of a user '''
+
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
@@ -25,5 +28,13 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        ''' init user '''
+
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        ''' sets password with md5 encryption '''
+
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
